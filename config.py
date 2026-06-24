3e39a4f8d9be4f9df92d4e1145e4470dd708107c
DHANUSHKUMAR T <t.dhanu2216@gmail.com>
2026-06-24 20:42:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,12 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = "dev_key_123"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev_key_123")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "sqlite:///" + os.path.join(BASE_DIR, "app.db")
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = False
-
